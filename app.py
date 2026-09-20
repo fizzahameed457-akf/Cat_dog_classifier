2103d5fab792d0a6bc51bcdc00b7498be69584bd
fizzahameed457-akf <fizzahameed457@gmail.com>
2026-09-20 18:25:57 +0500

    app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
-    st.image(image, caption='Your Image', use_column_width=True)
+    st.image(image, caption='Your Image', use_container_width=True)
 
     # ---- Preprocessing: EXACTLY like training ----
     img = image.resize((224, 224))
